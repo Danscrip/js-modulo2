ee0d5f913bc697c60f206e514f0597c644d86aba
david <dfyorio@gmail.com>
2023-03-22 17:08:57 -0300

    ultimotrabajo

--- script/data.js
+++ script/data.js
@@ -1,284 +1,127 @@
-var data = {
-    "currentDate": "2022-01-01",
-    "events": [
-      {
-        _id: 1,
-        "image":"https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
-        "name":"Collectivities Party",
-        "date":"2021-12-12",
-        "description":"Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-        "category":"Food Fair",
-        "place":"Room A",
-        "capacity":45000,
-        "assistance":42756,
-        "price":5
-      },
-      {
-        _id: 2,
-        "image":"https://i.postimg.cc/ZmD3Xf57/Korean-style.jpg",
-        "name":"Korean style",
-        "date":"2022-08-12",
-        "description":"Enjoy the best Korean dishes, with international chefs and awesome events.",
-        "category":"Food Fair",
-        "place":"Room A",
-        "capacity":45000,
-        "assistance":42756,
-        "price":10
-      },
-      {
-        _id: 3,
-        "image":"https://i.postimg.cc/GmHRkbNV/Jurassic-Park.jpg",
-        "name":"Jurassic Park",
-        "date":"2021-11-02",
-        "description":"Let's go meet the biggest dinosaurs in the paleontology museum.",
-        "category":"Museum",
-        "place":"Field",
-        "capacity":82000,
-        "assistance":65892,
-        "price":15
-      },
-      {
-        _id: 4,
-        "image":"https://i.postimg.cc/c4C2zXm8/Parisian-Museum.jpg",
-        "name":"Parisian Museum",
-        "date":"2022-11-02",
-        "description":"A unique tour in the city of lights, get to know one of the most iconic places.",
-        "category":"Museum",
-        "place":"Paris",
-        "capacity":8200,
-        "estimate":8200,
-        "price":3500
-      },
-      {
-        _id: 5,
-        "image":"https://i.postimg.cc/KYD0jMf2/comicon.jpg",
-        "name":"Comicon",
-        "date":"2021-02-12",
-        "description":"For comic lovers, all your favourite characters gathered in one place.",
-        "category":"Costume Party",
-        "place":"Room C",
-        "capacity":120000,
-        "assistance":110000,
-        "price":54
-      },
-      {
-        _id: 6,
-        "image":"https://i.postimg.cc/RZ9fH4Pr/halloween.jpg",
-        "name":"Halloween Night",
-        "date":"2022-02-12",
-        "description":"Come with your scariest costume and win incredible prizes.",
-        "category":"Costume Party",
-        "place":"Room C",
-        "capacity":12000,
-        "estimate":9000,
-        "price":12
-      },
-      {
-        _id: 7,
-        "image":"https://i.postimg.cc/PrMJ0ZMc/Metallica-in-concert.jpg",
-        "name":"Metallica in concert",
-        "date":"2022-01-22",
-        "description":"The only concert of the most emblematic band in the world.",
-        "category":"Music Concert",
-        "place":"Room A"
-        ,"capacity":138000,
-        "estimate":138000,
-        "price":150
-      },
-      {
-        _id: 8,
-        "image":"https://i.postimg.cc/KvsSK8cj/Electronic-Fest.jpg",
-        "name":"Electronic Fest",
-        "date":"2021-01-22",
-        "description":"The best national and international DJs gathered in one place.",
-        "category":"Music Concert",
-        "place":"Room A",
-        "capacity":138000,
-        "assistance":110300,
-        "price":250
-        },
-      {
-        _id: 9,
-        "image":"https://i.postimg.cc/fyLqZY9K/10-K-for-life.jpg",
-        "name":"10K for life",
-        "date":"2021-03-01",
-        "description":"Come and exercise, improve your health and lifestyle.",
-        "category":"Race",
-        "place":"Soccer field",
-        "capacity":30000,
-        "assistance":25698,
-        "price":3
-      },
-      {
-        _id: 10,
-        "image":"https://i.postimg.cc/zv67r65z/15kny.jpg",
-        "name":"15K NY",
-        "date":"2022-03-01",
-        "description":"We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
-        "category":"Race",
-        "place":"New York",
-        "capacity":3000000,
-        "assistance":2569800,
-        "price":3
-        },
-      {
-        _id: 11,
-        "image":"https://i.postimg.cc/Sst763n6/book1.jpg",
-        "name":"School's book fair",
-        "date":"2022-10-15",
-        "description":"Bring your unused school book and take the one you need.",
-        "category":"Book Exchange",
-        "place":"Room D1",
-        "capacity":150000,
-        "estimate":123286,
-        "price":1
-      },
-      {
-        _id: 12,
-        "image":"https://i.postimg.cc/05FhxHVK/book4.jpg",
-        "name":"Just for your kitchen",
-        "date":"2021-11-09",
-        "description":"If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
-        "category":"Book Exchange",
-        "place":"Room D6",
-        "capacity":130000,
-        "assistance":90000,
-        "price":100
-      },
-      {
-        _id: 13,
-        "image":"https://i.postimg.cc/vH52y81C/cinema4.jpg",
-        "name":"Batman",
-        "date":"2021-03-11",
-        "description":"Come see Batman fight crime in Gotham City.",
-        "category":"Cinema",
-        "place":"Room D1",
-        "capacity":11000,
-        "assistance":9300,
-        "price":225
-      },
-      {
-        _id: 14,
-        "image":"https://i.postimg.cc/T3C92KTN/scale.jpg",
-        "name":"Avengers",
-        "date":"2022-10-15",
-        "description":"Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
-        "category":"Cinema",
-        "place":"Room D1",
-        "capacity":9000,
-        "estimate":9000,
-        "price":250
-      }
-    ]
-  };
+let urlAPI = 'https://mindhub-xj03.onrender.com/api/amazing'
 
+let  datos = []
+let catern =[]
+let categorias =[]
+let checkboxButtons=[]
+let checkboxSeleccionados = [];
+let coincidenciasArray = []
 
+async function getEventos (){
+   
 
-
-
-  /*const datos=data.events
-console.log(datos)
-
- 
-  const init = function chance (){
-  const clone=document.getElementById('card').content
-  const lista = document.getElementById('contenedor')
-  const fragment = document.createDocumentFragment()
-  for (let task of datos ){
-    clone.querySelector('.card-title').textContent = task.name  
-  clone.querySelector('.card-text').textContent = task.description
-  clone.querySelector('img').setAttribute('src',task.image)
-  
-  const reflow =lista.appendChild(clone.cloneNode(true))
-  fragment.appendChild(reflow)
-    
-  }
-  }
-  document.addEventListener('DOMContentLoaded', init)
-
-console.log(3+4)
-*/
-let fechas = data.events
-let datos = data.events
-
-const time = data.currentDate
-let categoria = datos.category
-document.getElementsByClassName('card');
-
-
-let checkboxButtons = document.querySelectorAll(".checkbox")
-console.log(checkboxButtons)
-checkboxButtons.forEach(checkbox => checkbox.addEventListener("change", filtrarCards))
-
-let contenedorCheck = document.getElementById("contenedorCheck")
-
-//esta funcion recibe como un array y crea las cards con esa informacion
-function crearCartas(arr ) {
-
-  let lista = document.getElementById("contenedor")
-let clone = document.getElementById("card").content
-  lista.innerHTML = ""
-  
-  const fragment = document.createDocumentFragment()
-  for (let task of arr) {
-    //if ( time>task.date&&e.target. id==task.category ){
-
-    clone.querySelector('.card-title').textContent = task.name
-    clone.querySelector('.categorys').textContent = task.category
-    clone.querySelector('.card-text').textContent = task.description
-    clone.querySelector('img').setAttribute('src', task.image)
-    const impostores = clone.cloneNode(true)
-
-
-
-
-    fragment.appendChild(impostores)
-
-
-    // } 
+    try {
+        const response = await fetch(urlAPI)
+        const dataAPI = await response.json()
+console.log(dataAPI);
+console.log(datos);
+       
+      
     
 
-  }
-  lista.appendChild(fragment)
-}
+
+        datos.push(dataAPI)
+                  console.log(dataAPI.events)
+                  console.log(datos[0].events)
+        
 
 
 
-let checkboxSeleccionados = [] ;
-function filtrarCards (e){
-  if (e.target.checked){
-    checkboxSeleccionados.push(e.target.id)
-    filtrarCardsCheckbox(data.events , checkboxSeleccionados)
-  }else{
-    let deseleccionados = checkboxSeleccionados.filter(item => item!== e.target.id)
-    checkboxSeleccionados = deseleccionados
-    filtrarCardsCheckbox(data.events , checkboxSeleccionados)
-  }
-console.log(checkboxSeleccionados)
-console.log(datos)
-console.log(e.target.id)
-//crearCartas(checkboxSeleccionados)
-}
-let coincidenciasArray=[]
-function filtrarCardsCheckbox(arr , arrInputs){
-  if (arrInputs.length===0){
-    crearCartas(datos)
-  }else {
-    coincidenciasArray=[]
-  arr.forEach(element => {
-    for (const input of arrInputs) {
-      if (element.category==input){
-        coincidenciasArray.push(element)
-        //console.log("entro al if")
-      }
+        crearCartas(datos)
+        
+        
+  categorias = extraerCategorias(datos);
+        
+checkboxesrender(categorias)
+
+
+      
+        logic(datos)
+        consul (datos)
+    } catch (error) {
+        console.log(error.message)
     }
-  })
-  console.log(coincidenciasArray)
-  crearCartas(coincidenciasArray)
-  } 
 }
 
-crearCartas(datos)
+getEventos();
+
+
+
+  function consul (arr){
+    console.log(arr[0].events)
+    
+  }
+
+
+
+//console.log(dataAPI)
+function logic (dato){
+   
+    console.log(dato[0].events)
+    
+    }
+
+
+    function crearCartas(datos ) {
+
+
+
+        const time = datos[0].currentDate
+        console.log(datos[0])
+        
+        
+          let lista = document.getElementById("contenedor")
+        let clone = document.getElementById("card").content
+          lista.innerHTML = ""
+          
+          const fragment = document.createDocumentFragment()
+          for (let task of datos[0].events) {
+          
+               
+            clone.querySelector('.card-title').textContent = task.name
+            clone.querySelector('.categorys').textContent = task.category
+            clone.querySelector('.card-text').textContent = task.description
+            clone.querySelector('img').setAttribute('src', task.image)
+            clone.querySelector('a').setAttribute('href', `details.html?id=${task._id}` )
+            const impostores = clone.cloneNode(true)
+        
+        
+        {}
+            fragment.appendChild(impostores)
+        
+        
+              
+            
+        
+          }
+          lista.appendChild(fragment)
+        }
+
+        
+function extraerCategorias(parametro){
+let categoria = [];
+
+//let time = parametro[0].currentDate
+for (const cate of parametro[0].events) {
+ 
+   if ( !categoria.includes(cate.category) ){
+      categoria.push(cate.category);
+      
+    }
+  
+}
+
+console.log(categoria);
+return categoria
+
+}
+//console.log(categorias)
+///return categorias
+
+
+// if (time<cate.date)
+   // categorias.push(cate.category)
+//console.log(categorias);
 
 
 
